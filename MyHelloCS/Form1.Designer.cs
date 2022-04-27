f4a124b2758b56ddb24f0fc26428fa2ef5f759e3
yoonjuhee22 <s2014@e-mirim.hs.kr>
2022-04-27 10:00:45 +0900

    #2 환영 인사 버튼 추가 Greeting으로 해주었다

--- MyHelloCS/Form1.Designer.cs
+++ MyHelloCS/Form1.Designer.cs
@@ -28,13 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnGreeting = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnGreeting
+            // 
+            this.btnGreeting.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGreeting.Font = new System.Drawing.Font("궁서체", 17F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnGreeting.Location = new System.Drawing.Point(36, 48);
+            this.btnGreeting.Name = "btnGreeting";
+            this.btnGreeting.Size = new System.Drawing.Size(177, 86);
+            this.btnGreeting.TabIndex = 0;
+            this.btnGreeting.Text = "환영 인사하기";
+            this.btnGreeting.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("궁서체", 17F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(36, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 86);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "환영 인사하기";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("궁서체", 17F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button2.Location = new System.Drawing.Point(36, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(196, 100);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "환영 인사하기";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(268, 197);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnGreeting);
             this.Name = "FormMain";
             this.Text = "마이헬로우";
             this.ResumeLayout(false);
@@ -42,6 +81,10 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGreeting;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
